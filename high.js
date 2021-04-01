51c1d1c6397b5df9e18ccd602267edf787e32f07
S005956 <1833411957@qq.com>
2021-04-01 11:08:29 +0800

    异步js

---
+++ high.js
@@ -0,0 +1,23 @@
+//新建promise
+function now(params) {
+  console.log("55");
+}
+function myAsyn(params) {
+  return new Promise(() => {
+    console.log("2");
+  });
+}
+myAsyn().then(now());
+
+// async返回promise
+async function hello() {
+  return "Hello";
+}
+hello().then(console.log);
+hello().then((value)=>console.log(value))
+//await
+async function helloWorld() {
+  return greeting = await Promise.resolve("helloWorld");
+};
+
+helloWorld().then(console.log);
--- js-coding.js
+++ js-coding.js
@@ -101,14 +101,14 @@ console.log(new Date().toString());
 const time = Date.parse(new Date().toString()) / 1000 / 60 / 60;
 console.log(time);
 // 异步操作
-function timeout(ms) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, ms, "done");
-  });
-}
-timeout(1000).then((value) => {
-  console.log(value);
-});
+// function timeout(ms) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(resolve, ms, "done");
+//   });
+// }
+// timeout(1000).then((value) => {
+//   console.log(value);
+// });
 console.log("---------------------");
 var o = {};
 Object.defineProperty(o, "a", { value: 1, enumerable: true });
@@ -129,3 +129,92 @@ for (var i in o) {
 }
 console.log(Object.keys(o));
 console.log("---------------------");
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.question("你如何看待 Node.js 中文网？", (answer) => {
+//   // TODO：将答案记录在数据库中。
+//   console.log(`感谢您的宝贵意见：${answer}`);
+
+//   rl.close();
+// });
+// 执行顺序示例：
+console.log("aa"); // 第1步输出 aa
+async function test() {
+  console.log("bb"); // 第2步输出 bb
+  const res = await 222;
+  console.log(res); // 第4步输出 222
+  console.log("dd"); // 第5步输出 dd
+  return res;
+}
+test().then((res) => console.log(res)); // 第6步输出 222
+setTimeout(() => {
+  console.log("ff"); // 第7步输出 ff
+}, 500);
+console.log("cc"); // 第3步输出 cc
+
+let bluebird = require("bluebird");
+let fs = require("fs");
+let read = bluebird.promisify(fs.readFile);
+//await 命令后面的 Promise 对象，运行结果可能是 //rejected，所以最好把 await 命令放在 try...catch 代码块中。
+
+async function r() {
+  try {
+    let content1 = await read("text.txt", "utf8");
+    let content2 = await read(content1, "utf8");
+    return content2;
+  } catch (e) {
+    console.log("err", e);
+  }
+}
+
+r().then(
+  function (data) {
+    console.log("data", data);
+  },
+  function (err) {
+    console.log("err1", err);
+  }
+);
+(() => {
+  var date = new Date();
+  this.year = date.getFullYear();
+  this.month = date.getMonth() + 1;
+  this.date = date.getDate();
+  this.day = new Array(
+    "星期日",
+    "星期一",
+    "星期二",
+    "星期三",
+    "星期四",
+    "星期五",
+    "星期六"
+  )[date.getDay()];
+  this.hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  this.minute =
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  this.second =
+    date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+  var currentTime =
+    "现在是:" +
+    this.year +
+    "年" +
+    this.month +
+    "月" +
+    this.date +
+    "日 " +
+    this.hour +
+    ":" +
+    this.minute +
+    ":" +
+    this.second +
+    " " +
+    this.day;
+  console.log(currentTime);
+})();
+var d = new Date(2019, 5, 0);
+console.log(d.getDate());
