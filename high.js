20ceec3412903550e6864e709bc535942ee63518
S005956 <1833411957@qq.com>
2021-04-14 14:16:19 +0800

    svg和图片

--- high.js
+++ high.js
@@ -1,8 +1,8 @@
 //新建promise
-function now(params) {
+function now() {
   console.log("55");
 }
-function myAsyn(params) {
+function myAsyn() {
   return new Promise(() => {
     console.log("2");
   });
@@ -17,7 +17,19 @@ hello().then(console.log);
 hello().then((value) => console.log(value));
 //await
 async function helloWorld() {
-  return (greeting = await Promise.resolve("helloWorld"));
+  let greeting;
+  return (greeting = "helloWorld");
 }
 
 helloWorld().then(console.log);
+
+// const promise = new Promise(function(resolve, reject) {
+//   resolve('ok');
+//   throw new Error('test');
+// });
+// promise
+//     .then(function(value) { console.log(value) })
+//     .catch(function(error) { console.log(error) });
+// ok
+let user = [];
+console.log(user[0]?.addresss);
